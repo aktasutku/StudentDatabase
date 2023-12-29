a46a48632875b0b9ae13c2e4db815dd3aba5d155
utku <mattaktas94@gmail.com>
2023-12-29 11:27:37 -0500

    courses HashSet Added, payTuition added

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -1,9 +1,12 @@
 package studentdatabaseapp;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Student {
-    private String firstName, lastName, courses=null, studentID;
+    private String firstName, lastName, studentID;
+    HashSet<String> coursesSet = new HashSet<>();
     private int gradeYear, tuitionBalance = 0;
     private static int costOfCourse = 600;
     private static int id=1000;
@@ -11,13 +14,13 @@ public class Student {
     public Student(){
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter student first name: ");
+        System.out.print("Enter student first name: ");
         this.firstName = in.nextLine();
 
-        System.out.println("Enter student last name: ");
+        System.out.print("Enter student last name: ");
         this.lastName = in.nextLine();
 
-        System.out.println("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter students class level: ");
+        System.out.print("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter students class level: ");
         this.gradeYear = in.nextInt();
         setStudentID();
         System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
@@ -32,6 +35,7 @@ public class Student {
 
     //Enroll in courses
     public void enroll(){
+
         //Get inside a loop,user hits 0
         do{
         System.out.print("Enter course to enroll(Q to quit): ");
@@ -39,20 +43,35 @@ public class Student {
         String course = in.nextLine();
 
         if(!course.equals("Q")){
-            courses = courses +"\n" + course;
+            if(!course.equals("") && coursesSet.add(course)){
+                System.out.println(course + " Added");
+            }else{
+                System.out.println(course + " already added");
+            }
             tuitionBalance = tuitionBalance + costOfCourse;
         }  else break;
 
         }while(true);
 
 
-        System.out.println("Enrolled in: " + courses);
+        System.out.println("Enrolled in: " + coursesSet);
         System.out.println("Tuition balance: " + tuitionBalance);
     }
 
     // View balance
+    public void viewBalance(){
+        System.out.println("Your current balance is: $"+ tuitionBalance);
+    }
 
     //Pay Tuition
+    public void payTuition(){
+        System.out.print("Enter your payment: $");
+        Scanner in = new Scanner(System.in);
+        int payment = in.nextInt();
+        tuitionBalance = tuitionBalance - payment;
+        System.out.println("Thank you for your payment of $" + payment);
+        viewBalance();
+    }
 
     //Show status
 
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -4,6 +4,7 @@ public class StudentDatabaseApp {
     public static void main(String[] args) {
         Student stud1 = new Student();
         stud1.enroll();
+        stud1.payTuition();
 
         //Ask how many new users we want to add
 
